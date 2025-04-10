454d9dbfe5a32e7e44d075f2a80da639beb63b52
shulksteve <shulk.work@gmail.com>
2025-04-10 14:10:35 +0800

    init

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from 'next';
+import React from 'react';
+import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'React + Next.js + ShadcnUI',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
